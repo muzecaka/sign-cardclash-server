88c31c06210d1299ff33854b3056d03ffb382304
muzecaka <your-muzecaka-email@gmail.com>
2025-06-21 03:36:03 +0100

    Update CLIENT_URL for production

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5000", "https://sign-cardclash.vercel.app"],
+    origin: ["http://localhost:5000", "https://sign-cardclash.netlify.app"],
     methods: ["GET", "POST"],
   },
 });
