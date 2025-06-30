9cd539fdec3254ce6c34a7a84ce6277fb5446f00
muzecaka <your-muzecaka-email@gmail.com>
2025-06-30 06:32:57 +0100

    server URL

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000", "https://cardclash.vercel.app"],
+    origin: ["http://localhost:3000", "https://sign-cardclash.vercel.app"],
     methods: ["GET", "POST"],
   },
 });
